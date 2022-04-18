fb74c048ba9fa4fb370f4ec54e80a24b44fdd6c3
noman-sheikh-09 <nomanshk310@gmail.com>
2022-04-19 02:49:04 +0500

    quote generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [quote, setQuote] = useState([]);
+  useEffect(() => {
+    let data = async () => {
+      let resValue = await axios.get("http://api.quotable.io/random");
+      setQuote(resValue.data);
+    };
+    data();
+  }, []);
+  const fetchData = async () => {
+    let resValue = await axios.get("http://api.quotable.io/random");
+    setQuote(resValue.data);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card-bg">
+        <div className="card text-center my-5">
+          <div className="card-body">
+            <h5 className="card-title">{quote.author}</h5>
+            <p className="card-text">{quote.content}</p>
+            <button className="btn btn-primary" onClick={fetchData}>
+              Go Next
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
